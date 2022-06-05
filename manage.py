1f3bf7ade78302923fea5f8947a2b01462c6d395
Jitendra Sharma <jitendra.sharma2020@outlook.com>
2022-06-05 11:43:00 +0530

    manage.py updated

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from app.main import create_app
 
 # create app using create_app() function having 'dev' environment
 app = create_app('dev')
-app.register_blueprint(api_bp, url_prefix="/api/v1")
+app.register_blueprint(blueprint, url_prefix="/api/v1")
 
 
 # run application
